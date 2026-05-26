bee148137967779fea4b195adf52a0eb245afc3d
Keyotic <keyon.vanstichel@hotmail.com>
2026-05-26 17:21:40 +0200

    changes in main page

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -53,7 +53,7 @@ function Card({ item, onLike, onSkip }) {
     dragX < 0 ? 1 + clamp(Math.abs(dragX) / intensity, 0, maxGrow) : 1;
 
   return (
-    <div style={{ width: 1026, overflow: "hidden", marginLeft: "-44.3em", }}>
+    <div style={{ width: 1026, overflow: "hidden", marginLeft: "-66.3em", }}>
 
       {/* SWIPING AREA — THIS MOVES */}
       <div
