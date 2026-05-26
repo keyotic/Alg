e77fa13c9a4256e7bbea10bfb5f1005fcc24336e
Keyotic <keyon.vanstichel@hotmail.com>
2026-05-26 17:18:26 +0200

    center being centered test

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -53,7 +53,7 @@ function Card({ item, onLike, onSkip }) {
     dragX < 0 ? 1 + clamp(Math.abs(dragX) / intensity, 0, maxGrow) : 1;
 
   return (
-    <div style={{ width: 1024, overflow: "hidden", marginLeft: "-44em", }}>
+    <div style={{ width: 1026, overflow: "hidden", marginLeft: "-44.3em", }}>
 
       {/* SWIPING AREA — THIS MOVES */}
       <div
